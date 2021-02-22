9f53391e7cdf138b5c864009edda12c1d28888d1
haben2022 <berhanetesfahannes11@gmail.com>
2021-02-22 15:57:24 -0500

    Create firstprogram.js
    i did my first sample program

---
+++ firstprogram.js
@@ -0,0 +1,11 @@
+function notMultipleOf3(arr){
+    newarr=[];
+    for(var i=0; i<arr.length; i++){
+      if(arr[i]%3!==0){
+        newarr.push(arr[i]);
+    }
+    }
+    return newarr;
+    }
+    oldarr=[1,2,3,4,5,6,7,9]; 
+    console.log(notMultipleOf3(oldarr));
