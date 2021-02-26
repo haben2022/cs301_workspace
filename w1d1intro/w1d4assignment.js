d49ac8d28d62f992f77b29927ba11d842c31fc56
haben2022 <berhanetesfahannes11@gmail.com>
2021-02-26 00:21:47 -0500

    assignement week1day4

---
+++ w1d1intro/w1d4assignment.js
@@ -0,0 +1,17 @@
+const prompt=require('prompt-sync')();
+let num=+prompt("please enter number :");
+    let i=2;
+    let output=false;
+while (i<=num) {
+    if(num%i!=0){
+        if(num===i){
+            console.log(num); 
+        }
+    
+    
+    }
+    i++;
+    
+}
+ num=parseInt(prompt("please enter number :"));
+
